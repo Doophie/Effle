adf1532550161f17d4a569eea52410debb9c09c2
qfagan <qfagan@bluink.ca>
2019-05-10 11:12:28 -0400

    basic encryption

--- doophrame/src/main/java/ca/doophie/doophrame/extensions/Intent.kt
+++ doophrame/src/main/java/ca/doophie/doophrame/extensions/Intent.kt
@@ -1,16 +1,16 @@
 package ca.doophie.doophrame.extensions
 
 import android.content.Intent
-import ca.doophie.doophrame.models.ObjectSerializer
+import ca.doophie.doophrame.models.DoophieObjectSerializer
 import java.io.Serializable
 
 // TODO: Handle encryption / decryption here
 
 fun <T: Serializable>Intent.getObject(key: String) : T? {
     if (this.extras == null) return null
-    return ObjectSerializer.deserialize(this.extras.getString(key))
+    return DoophieObjectSerializer.deserialize(this.extras.getString(key))
 }
 
 fun <T: Serializable>Intent.putObject(key: String, obj: T) {
-    this.putExtra(key, ObjectSerializer.serialize(obj))
+    this.putExtra(key, DoophieObjectSerializer.serialize(obj))
 }
