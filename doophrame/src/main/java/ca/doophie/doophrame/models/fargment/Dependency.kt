d4867a89f43ebc186bd6984726eb9ded23a166ff
qfagan <qfagan@bluink.ca>
2019-05-11 12:14:11 -0400

    work

---
+++ doophrame/src/main/java/ca/doophie/doophrame/models/fargment/Dependency.kt
@@ -0,0 +1,7 @@
+package ca.doophie.doophrame.models.fargment
+
+import java.io.Serializable
+
+class Dependency: Serializable {
+
+}
--- doophrame/src/main/java/ca/doophie/doophrame/models/fargment/DoophieWorker.kt
+++ doophrame/src/main/java/ca/doophie/doophrame/models/fargment/DoophieWorker.kt
@@ -1,2 +1,23 @@
 package ca.doophie.doophrame.models.fargment
 
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class DoophieWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+
+
+
+        return Result.success()
+    }
+
+    fun <T: ((Any)->Result)>executeTask(task: T, params: Any) {
+        task(params)
+    }
+
+}
+
+
